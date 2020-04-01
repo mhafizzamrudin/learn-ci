cb02fd9d3be3cf4b6705275e8b8c1d336986f67d
adityaeka26 <aditya.eka409@gmail.com>
2020-04-02 01:52:52 +0700

    update

--- application/views/RegisterView.php
+++ application/views/RegisterView.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Page Title - SB Admin</title>
+        <title>Register</title>
         <link href="<?=base_url('assets/sbadmin/')?>css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
     </head>
